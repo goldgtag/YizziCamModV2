486132844dffb56a8422d83452946275502d1055
goldgtag <goldskullvr@gmail.com>
2026-05-20 22:43:01 -0400

    StartPatch.cs

--- Patches/StartPatch.cs
+++ Patches/StartPatch.cs
@@ -1,16 +1,19 @@
-﻿using HarmonyLib;
+using System;
+using HarmonyLib;
 using UnityEngine;
-using YizziCamModV2;
+
 namespace YizziCamModV2.Patches
 {
-    [HarmonyPatch(typeof(GorillaTagger))]
-    [HarmonyPatch("Start", MethodType.Normal)]
-    internal class StartPatch
-    {
-        private static void Postfix()
-        {
-            new GameObject().AddComponent<CameraController>();
-            CameraController.Instance.YizziStart();
-        }
-    }
+	// Token: 0x02000007 RID: 7
+	[HarmonyPatch(typeof(GorillaTagger))]
+	[HarmonyPatch("Start", 0)]
+	internal class StartPatch
+	{
+		// Token: 0x0600000E RID: 14 RVA: 0x000020D8 File Offset: 0x000002D8
+		private static void Postfix()
+		{
+			new GameObject().AddComponent<CameraController>();
+			CameraController.Instance.YizziStart();
+		}
+	}
 }
