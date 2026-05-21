e5e7210765bef578e3043cf648cefa6e7c472963
goldgtag <goldskullvr@gmail.com>
2026-05-20 22:40:05 -0400

    HarmonyPatches.cs

--- HarmonyPatches.cs
+++ HarmonyPatches.cs
@@ -4,33 +4,42 @@ using HarmonyLib;
 
 namespace YizziCamModV2
 {
+	// Token: 0x02000004 RID: 4
 	public class HarmonyPatches
 	{
-		private static Harmony instance;
-
+		// Token: 0x17000001 RID: 1
+		// (get) Token: 0x06000006 RID: 6 RVA: 0x00002058 File Offset: 0x00000258
+		// (set) Token: 0x06000007 RID: 7 RVA: 0x0000205F File Offset: 0x0000025F
 		public static bool IsPatched { get; private set; }
-		public const string InstanceId = PluginInfo.GUID;
 
+		// Token: 0x06000008 RID: 8 RVA: 0x00002067 File Offset: 0x00000267
 		internal static void ApplyHarmonyPatches()
 		{
-			if (!IsPatched)
+			if (!HarmonyPatches.IsPatched)
 			{
-				if (instance == null)
+				if (HarmonyPatches.instance == null)
 				{
-					instance = new Harmony(InstanceId);
+					HarmonyPatches.instance = new Harmony("com.yizzi.gorillatag.yizzicammodv2");
 				}
-
-				instance.PatchAll(Assembly.GetExecutingAssembly());
-				IsPatched = true;
+				HarmonyPatches.instance.PatchAll(Assembly.GetExecutingAssembly());
+				HarmonyPatches.IsPatched = true;
 			}
 		}
+
+		// Token: 0x06000009 RID: 9 RVA: 0x0000209B File Offset: 0x0000029B
 		internal static void RemoveHarmonyPatches()
 		{
-			if (instance != null && IsPatched)
+			if (HarmonyPatches.instance != null && HarmonyPatches.IsPatched)
 			{
-				instance.UnpatchSelf();
-				IsPatched = false;
+				HarmonyPatches.instance.UnpatchSelf();
+				HarmonyPatches.IsPatched = false;
 			}
 		}
+
+		// Token: 0x04000034 RID: 52
+		private static Harmony instance;
+
+		// Token: 0x04000036 RID: 54
+		public const string InstanceId = "com.yizzi.gorillatag.yizzicammodv2";
 	}
 }
