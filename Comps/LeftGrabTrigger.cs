1fad17577647caccd8fbcfaad9cfd9aea6641274
goldgtag <goldskullvr@gmail.com>
2026-05-20 22:44:29 -0400

    LeftGrabTrigger.cs

--- Comps/LeftGrabTrigger.cs
+++ Comps/LeftGrabTrigger.cs
@@ -1,27 +1,32 @@
-﻿using UnityEngine;
-#pragma warning disable CS0618
+using System;
+using UnityEngine;
+
 namespace YizziCamModV2.Comps
 {
-    class LeftGrabTrigger : MonoBehaviour
-    {
-        void Start()
-        {
-            gameObject.layer = 18;
-        }
-        void OnTriggerStay(Collider col)
-        {
-            if (col.name.Contains("Left"))
-            {
-                if (InputManager.instance.LeftGrip & !CameraController.Instance.fpv)
-                {
-                    CameraController.Instance.CameraTablet.transform.parent = CameraController.Instance.LeftHandGO.transform;
-                    if (CameraController.Instance.fp) { CameraController.Instance.fp = false; }
-                }
-            }
-            if (!InputManager.instance.LeftGrip & CameraController.Instance.CameraTablet.transform.parent == CameraController.Instance.LeftHandGO.transform)
-            {
-                CameraController.Instance.CameraTablet.transform.parent = null;
-            }
-        }
-    }
+	// Token: 0x02000009 RID: 9
+	internal class LeftGrabTrigger : MonoBehaviour
+	{
+		// Token: 0x06000013 RID: 19 RVA: 0x0000210D File Offset: 0x0000030D
+		private void Start()
+		{
+			base.gameObject.layer = 18;
+		}
+
+		// Token: 0x06000014 RID: 20 RVA: 0x00003064 File Offset: 0x00001264
+		private void OnTriggerStay(Collider col)
+		{
+			if (col.name.Contains("Left") && (InputManager.instance.LeftGrip & !CameraController.Instance.fpv))
+			{
+				CameraController.Instance.CameraTablet.transform.parent = CameraController.Instance.LeftHandGO.transform;
+				if (CameraController.Instance.fp)
+				{
+					CameraController.Instance.fp = false;
+				}
+			}
+			if (!InputManager.instance.LeftGrip & CameraController.Instance.CameraTablet.transform.parent == CameraController.Instance.LeftHandGO.transform)
+			{
+				CameraController.Instance.CameraTablet.transform.parent = null;
+			}
+		}
+	}
 }
