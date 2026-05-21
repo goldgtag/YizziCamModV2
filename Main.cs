0922c324972b2ef618253e21c4ebcc073b63e2ee
goldgtag <goldskullvr@gmail.com>
2026-05-20 22:40:34 -0400

    Main.cs

--- Main.cs
+++ Main.cs
@@ -1,22 +1,18 @@
-﻿using System.Collections.Generic;
-using System.IO;
-using System.Reflection;
+using System;
 using BepInEx;
-using Cinemachine;
-using GorillaLocomotion;
 using UnityEngine;
-using UnityEngine.UI;
-using YizziCamModV2.Comps;
-#pragma warning disable CS0618
+
 namespace YizziCamModV2
 {
-    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
-    public class Main : BaseUnityPlugin
-    {
-        void Awake()
-        {
-            HarmonyPatches.ApplyHarmonyPatches();
-            DontDestroyOnLoad(this);
-        }
-    }
+	// Token: 0x02000005 RID: 5
+	[BepInPlugin("com.yizzi.gorillatag.yizzicammodv2", "YizziCamModV2", "1.0.8")]
+	public class Main : BaseUnityPlugin
+	{
+		// Token: 0x0600000B RID: 11 RVA: 0x000020C3 File Offset: 0x000002C3
+		private void Awake()
+		{
+			HarmonyPatches.ApplyHarmonyPatches();
+			Object.DontDestroyOnLoad(this);
+		}
+	}
 }
